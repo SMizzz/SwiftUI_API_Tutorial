3b8547e2d4de56928e77418c19fdae42ba1cfdac
SMizzz <shinn.mizzz@gmail.com>
2021-09-29 11:33:15 +0900

    git init

---
+++ SwiftUI_API_Tutorial/SwiftUI_API_TutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_API_TutorialApp.swift
+//  SwiftUI_API_Tutorial
+//
+//  Created by 신미지 on 2021/09/29.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_API_TutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
